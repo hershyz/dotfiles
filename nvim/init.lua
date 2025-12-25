449132cd80abaca22b73d6dfb1d91d8bd0136451
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2025-12-25 16:59:18 -0700

    git integration (gitsigns)

--- nvim/init.lua
+++ nvim/init.lua
@@ -162,6 +162,7 @@ ensure_plugin("https://github.com/windwp/nvim-autopairs", "nvim-autopairs")
 ensure_plugin("https://github.com/nvim-lualine/lualine.nvim", "lualine.nvim")
 ensure_plugin("https://github.com/nvim-tree/nvim-web-devicons", "nvim-web-devicons")
 ensure_plugin("https://github.com/MunifTanjim/nui.nvim", "nui.nvim")
+ensure_plugin("https://github.com/lewis6991/gitsigns.nvim", "gitsigns")
 
 -- Install neo-tree from v3.x branch
 local neotree_path = plugin_path .. "neo-tree.nvim"
@@ -337,6 +338,56 @@ require("nvim-treesitter.configs").setup({
   indent = { enable = true },
   incremental_selection = { enable = true },
 })
+
+require('gitsigns').setup {
+     signs = {
+       add          = { text = '┃' },
+       change       = { text = '┃' },
+       delete       = { text = '_' },
+       topdelete    = { text = '‾' },
+       changedelete = { text = '~' },
+       untracked    = { text = '┆' },
+     },
+     signs_staged = {
+       add          = { text = '┃' },
+       change       = { text = '┃' },
+       delete       = { text = '_' },
+       topdelete    = { text = '‾' },
+       changedelete = { text = '~' },
+       untracked    = { text = '┆' },
+     },
+     signs_staged_enable = true,
+     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+     numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+     linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+     word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+     watch_gitdir = {
+       follow_files = true
+        },
+     auto_attach = true,
+     attach_to_untracked = false,
+     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+     current_line_blame_opts = {
+       virt_text = true,
+       virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+       delay = 1000,
+       ignore_whitespace = false,
+       virt_text_priority = 100,
+       use_focus = true,
+     },
+     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+     sign_priority = 6,
+     update_debounce = 100,
+     status_formatter = nil, -- Use default
+     max_file_length = 40000, -- Disable if file is longer than this (in lines)
+     preview_config = {
+      -- Options passed to nvim_open_win
+         style = 'minimal',
+         relative = 'cursor',
+         row = 0,
+         col = 1
+   },
+}
 
 -- ==========================
 -- LSP setup
