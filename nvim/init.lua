dc97317a3b7a70335a6e0bea72efea2188910e1a
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2026-01-07 19:25:34 -0500

    remove auto-commenting

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,14 @@ vim.opt.mousemodel = "popup_setpos"
 vim.o.mouse = "a"
 vim.opt.wrap = false
 
+-- Remove auto-commenting
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
 -- ==========================
 -- macOS-style keymaps
 -- ==========================
