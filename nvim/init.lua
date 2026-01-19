c75a9f74d12775ddf52f2e3bfe53de2fcf1cda6f
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2026-01-19 14:24:17 -0500

    1 tab = 4 spaces

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.opt.selectmode = "mouse,key"
